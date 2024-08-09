51321462088b9b4a82d7116941f01a5c175e2b86
Rodrigo Gonzalez <rodrigo.emilianogv@gmail.com>
2024-08-08 23:30:13 -0600

    prueba git2

--- src/typescript/BasicFunction.tsx
+++ src/typescript/BasicFunction.tsx
@@ -9,6 +9,7 @@ const BasicFunction = () => {
   return (
     <div>
       <h3>Funciones</h3>
+      <h3>Este es un mensaje de prueba para git</h3>
       <span>el resultado de sumar {addTwoNumber(2,8) }</span>
     </div>
   )
